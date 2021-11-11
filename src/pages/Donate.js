9032ae5055b6e1c713df86799a4a285e5b7695ae
JoAnn R Siciliano <77905621+joannsiciliano@users.noreply.github.com>
2021-11-11 16:09:55 -0700

    donate widget added

--- src/pages/Donate.js
+++ src/pages/Donate.js
@@ -3,7 +3,25 @@ import React from "react";
 const Donate = () => {
   return (
     <div className="DonateWrapper">
-      <h1>Donate 💸</h1>
+      <div className="iFrameWrapper">
+        <iframe
+          title="donorbox"
+          src="https://donorbox.org/embed/toy-drive-3"
+          name="donorbox"
+          allowpaymentrequest="allowpaymentrequest"
+          seamless="seamless"
+          frameborder="0"
+          scrolling="no"
+          height="900px"
+          width="100%"
+          style={{
+            maxWidth: 500,
+            minWidth: 250,
+            maxHeight: "none",
+            marginTop: 200,
+          }}
+        ></iframe>
+      </div>
     </div>
   );
 };
