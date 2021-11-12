d8924c82075c2c26959a41749ef884670878b669
JoAnn R Siciliano <77905621+joannsiciliano@users.noreply.github.com>
2021-11-12 16:52:33 -0700

    added links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,19 @@
 import React from "react";
-
+import PCLogo from "../logo.png";
+import PCEmblem from "../production-club-logo.svg";
 export default function Footer() {
   return (
     <div className="FooterWrapper">
-      <div className="FooterText">
-        <h3>hello am footer</h3>
+      <div className="footerImages">
+        <img src={PCLogo} alt="production club" className="PCLogo" />
+        <a
+          href="https://www.production.club"
+          target="_blank"
+          className="PCLink"
+        >
+          {" "}
+          <img src={PCEmblem} />{" "}
+        </a>
       </div>
     </div>
   );
