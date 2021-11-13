66b0a3da6c35dde9a0d8a578afbc46a1d5ac5eb9
JoAnn R Siciliano <77905621+joannsiciliano@users.noreply.github.com>
2021-11-12 17:33:32 -0700

    volunteer copy added

--- src/pages/Volunteer.js
+++ src/pages/Volunteer.js
@@ -3,7 +3,11 @@ import React from "react";
 const Volunteer = () => {
   return (
     <div className="VolunteerWrapper">
-      <h1>Volunteer 🤝</h1>
+      <p>
+        The Do Something Toy Drive aims to give underserved children access to
+        nicer toys than you find at a lot of giveaways. With that goal in mind
+        we are not asking for toy donations but purely monetary ones.
+      </p>
     </div>
   );
 };
