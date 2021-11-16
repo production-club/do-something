3f9bfec46178bd7784860af97ae994d98e0c3737
JoAnn R Siciliano <77905621+joannsiciliano@users.noreply.github.com>
2021-11-16 15:41:31 -0700

    footer links added, delete cname file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,10 @@
 import React from "react";
 import PCLogo from "../logo.png";
 import PCEmblem from "../production-club-logo.svg";
+import LinkedIn from "../images/linkedin.png";
+import Instagram from "../images/instagram.png";
+import Facebook from "../images/facebook.png";
+import Twitter from "../images/twitter.png";
 export default function Footer() {
   return (
     <div className="FooterWrapper">
@@ -14,6 +18,40 @@ export default function Footer() {
         >
           {" "}
           <img src={PCEmblem} alt="pc logo" />{" "}
+        </a>
+      </div>
+      <div className="SocialMediaLinks">
+        <a
+          href="https://www.linkedin.com/company/productionclub/"
+          target="blank"
+          rel="noreferrer"
+        >
+          {" "}
+          <img src={LinkedIn} alt="linkedin logo" />
+        </a>
+        <a
+          href="https://www.instagram.com/productionclub/"
+          target="blank"
+          rel="noreferrer"
+        >
+          {" "}
+          <img src={Instagram} alt="instagram logo png" />
+        </a>
+        <a
+          href="https://www.facebook.com/ProductionClub/"
+          target="blank"
+          rel="noreferrer"
+        >
+          {" "}
+          <img src={Facebook} alt="facebook logo png" />
+        </a>
+        <a
+          href="https://twitter.com/productionclub/"
+          target="blank"
+          rel="noreferrer"
+        >
+          {" "}
+          <img src={Twitter} alt="twitter icon png" />
         </a>
       </div>
     </div>
