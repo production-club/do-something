5d3c87926cd87936161ec1e9a59b6c2de290ed85
JoAnn R Siciliano <77905621+joannsiciliano@users.noreply.github.com>
2021-11-16 18:10:27 -0700

    flex footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,51 +8,55 @@ import Twitter from "../images/twitter.png";
 export default function Footer() {
   return (
     <div className="FooterWrapper">
-      <div className="footerImages">
+      <div className="FooterFlex">
         <img src={PCLogo} alt="production club" className="PCLogo" />
-        <a
-          href="https://www.production.club"
-          target="_blank"
-          rel="noreferrer"
-          className="PCLink"
-        >
-          {" "}
-          <img src={PCEmblem} alt="pc logo" />{" "}
-        </a>
-      </div>
-      <div className="SocialMediaLinks">
-        <a
-          href="https://www.linkedin.com/company/productionclub/"
-          target="blank"
-          rel="noreferrer"
-        >
-          {" "}
-          <img src={LinkedIn} alt="linkedin logo" />
-        </a>
-        <a
-          href="https://www.instagram.com/productionclub/"
-          target="blank"
-          rel="noreferrer"
-        >
-          {" "}
-          <img src={Instagram} alt="instagram logo png" />
-        </a>
-        <a
-          href="https://www.facebook.com/ProductionClub/"
-          target="blank"
-          rel="noreferrer"
-        >
-          {" "}
-          <img src={Facebook} alt="facebook logo png" />
-        </a>
-        <a
-          href="https://twitter.com/productionclub/"
-          target="blank"
-          rel="noreferrer"
-        >
-          {" "}
-          <img src={Twitter} alt="twitter icon png" />
-        </a>
+        <div className="FooterInnerFlex">
+          <div className="PCLinkDiv">
+            <a
+              href="https://www.production.club"
+              target="_blank"
+              rel="noreferrer"
+              className="PCLink"
+            >
+              {" "}
+              <img src={PCEmblem} alt="pc logo" />{" "}
+            </a>
+          </div>
+          <div className="SMLinkDiv">
+            <a
+              href="https://www.linkedin.com/company/productionclub/"
+              target="blank"
+              rel="noreferrer"
+            >
+              {" "}
+              <img src={LinkedIn} alt="linkedin logo" />
+            </a>
+            <a
+              href="https://www.instagram.com/productionclub/"
+              target="blank"
+              rel="noreferrer"
+            >
+              {" "}
+              <img src={Instagram} alt="instagram logo png" />
+            </a>
+            <a
+              href="https://www.facebook.com/ProductionClub/"
+              target="blank"
+              rel="noreferrer"
+            >
+              {" "}
+              <img src={Facebook} alt="facebook logo png" />
+            </a>
+            <a
+              href="https://twitter.com/productionclub/"
+              target="blank"
+              rel="noreferrer"
+            >
+              {" "}
+              <img src={Twitter} alt="twitter icon png" />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
