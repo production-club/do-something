21192209e3a0370694b2744207273082d15a28e3
JoAnn R Siciliano <77905621+joannsiciliano@users.noreply.github.com>
2021-12-06 12:03:35 -0700

    updated hours

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -30,7 +30,7 @@ const Home = () => {
           <p>
           
              <p style={{color:'white', fontWeight:'bold'}} className={styles.deskp}>
-              6pm - 9pm. December 18th, 2021.
+              1pm - 9pm. December 18th, 2021.
              </p > <p style={{color:'white', fontWeight:'bold'}}className={styles.tabp}>6pm - 9pm <br/> December 18th, 2021.</p><p className={styles.deskp}> 1725 Naud st, Los Angeles, CA 90012 | Chinatown DTLA</p>
              <p className={styles.tabp}> 1725 Naud st, Los Angeles, CA 90012 <br/> Chinatown DTLA</p>
           </p>
