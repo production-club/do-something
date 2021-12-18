fd23537810572e1dfc28060e6baf5a8b236e10da
JoAnn R Siciliano <77905621+joannsiciliano@users.noreply.github.com>
2021-12-17 17:11:28 -0700

    tying up loose ends on nav

--- src/components/Navbar/Sidebar/SidebarElements.js
+++ src/components/Navbar/Sidebar/SidebarElements.js
@@ -56,7 +56,7 @@ export const SidebarLink = styled(LinkR)`
 
 export const SidebarMenu = styled.ul`
 /* padding:2em; */
-margin-top: 20vh;
+margin-top: 23vh;
   display: grid;
   grid-template-columns: 1fr;
   grid-template-rows: repeat(6, 80px);
@@ -64,5 +64,8 @@ margin-top: 20vh;
 
   @media screen and (max-width: 480px) {
     grid-template-rows: repeat(6, 60px);
+  }
+  @media screen and (min-width:660px){
+  grid-template-rows:repeat(6,40px);
   }
 `;
